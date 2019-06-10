edee4edb28692b3c89e4fd016ab32f8c2d53712c
Ursula Miller <ursula.miller@gmail.com>
2019-06-10 03:21:45 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   array = ["Think zero", "think 1", "think twice", "Think thrice"]
-  element = "Quatro"
+  element = "arrays!"
   array << element
   puts array
 end
